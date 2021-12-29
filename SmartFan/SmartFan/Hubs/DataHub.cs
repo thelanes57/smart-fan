998d5a780f6d5ebdbcf99b7e582f9aa5ea30930a
Bzelga <81263572+Bzelga@users.noreply.github.com>
2021-12-29 11:43:28 +0500

    добавлен метод для выключения устройства на linux

--- SmartFan/SmartFan/Hubs/DataHub.cs
+++ SmartFan/SmartFan/Hubs/DataHub.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
 using SmartFan.Data;
 using SmartFan.Device;
 using System.Threading.Tasks;
+using Unosquare.RaspberryIO;
 
 namespace SmartFan.Hubs
 {
     public class DataHub : Hub<IDataHub>
     {
         DeviceManager deviceManager;
+
         public DataHub(DeviceManager devManager)
         {
             this.deviceManager = devManager;
@@ -18,6 +21,20 @@ namespace SmartFan.Hubs
             deviceManager.SetData(parameter);
         }
 
+        public async Task<bool> Shotdown()
+        {
+            try
+            {
+                await Pi.ShutdownAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        
         public override async Task OnConnectedAsync()
         {
             await Clients.All.StartSpeed(deviceManager.ChangeParameter);
